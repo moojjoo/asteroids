6bf8966faf8fe631dad0b42bd002ad867915fe31
moojjoo <robertdannelly@gmail.com>
2025-06-03 16:45:04 -0400

    Dinner time

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import *
 
 class Player(CircleShape):
     def __init__(self, x, y, PLAYER_RADIUS):
@@ -63,6 +63,14 @@ class Player(CircleShape):
             self.rotation -= self.rotate(dt)
         if keys[pygame.K_d]:
             self.rotation += self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
 
 
 
