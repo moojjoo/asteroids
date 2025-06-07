94ea58f8928595e27a73b3c94e6df3d8acb33f34
moojjoo <robertdannelly@gmail.com>
2025-06-07 04:45:28 -0400

    Astoriods Finished

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -12,7 +14,28 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        # If the asteroid is too small, it cannot be split
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+        # Randomly choose an angle to  the velocity vector
+        else:        
+            random_angle = random.uniform(20, 50)
+            # Create two new velocity vectors, rotated in opposite directions
+            velocity1 = self.velocity.rotate(random_angle) * 1.2
+            velocity2 = self.velocity.rotate(-random_angle) * 1.2
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            # Create two new asteroids at the current position with the new radius
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity = velocity1
+            asteroid2.velocity = velocity2
+            self.kill()  # Remove the current asteroid
         
+
+
 
         
 
